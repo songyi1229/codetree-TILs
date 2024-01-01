0a6c4b754ab2218f37ffa89da6277405d0f71256
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-01-01 12:36:28 +0900

    Title: 4번의 크기 비교, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240101/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,26 @@
+a = int(input())
+b,c,d,e=input().split()
+b = int(b)
+c = int(c)
+d = int(d)
+e = int(e)
+
+if a>b:
+    print("1")
+else:
+    print("0")
+
+if a>c:
+    print("1")
+else:
+    print("0")
+
+if a>d:
+    print("1")
+else:
+    print("0")
+
+if a>e:
+    print("1")
+else:
+    print("0")
