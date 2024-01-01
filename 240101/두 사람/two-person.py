a0c79461fec94cb7be2884301b81eb776481578f
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-01-01 13:47:05 +0900

    Title: 두 사람, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240101/두 사람/two-person.py
+++ 240101/두 사람/two-person.py
@@ -3,7 +3,8 @@ a2, s2 = input().split()
 a1 = int(a1)
 a2 = int(a2)
 
-if (a1>=19 or a2>=19) and (s1=="M" or s2=="M"):
-    print('1')
+
+if (a1 >= 19 and s1=="M") or (a2 >= 19 and s2=="M"):
+    print("1")
 else:
     print("0")
