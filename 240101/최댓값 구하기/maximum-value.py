41c03a21517a4786228d2e8e98b3d24c3f6b9999
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-01-01 14:07:33 +0900

    Title: 최댓값 구하기, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240101/최댓값 구하기/maximum-value.py
+++ 240101/최댓값 구하기/maximum-value.py
@@ -3,9 +3,9 @@ a = int(a)
 b = int(b)
 c = int(c)
 
-if a>b and a>c:
+if a>=b and a>=c:
     print(a)
-elif b>a and b>c:
+elif b>=a and b>=c:
     print(b)
-elif c>a and c>b:
+elif c>=a and c>=b:
     print(c)
