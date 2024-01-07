99634cf79f36b02314ece48bb7e6e32de22a7082
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-01-08 00:05:31 +0900

    Title: 369 게임, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240108/369 게임/369-game.py
+++ 240108/369 게임/369-game.py
@@ -4,11 +4,9 @@ n = int(n)
 for i in range(1, n+1):
     if i%3==0 or arr == 3 or arr ==6 or arr==9:
         print(0, end =" ")
-    elif i==3 or i==6 or i==9:
+    elif i==3 or i==6 or i==9 or i%10==3 or i%10==6 or i%10==9 or i%100==3 or i%100==6 or i%100==9:
         print(0, end =" ")
-    elif i%10==3 or i%10==6 or i%10==9:
-        print(0, end =" ")
-    elif i%100==3 or i%100==6 or i%100==9:
+    elif i//10==3 or i//10==6 or i//10==9 or i//100==3:
         print(0, end =" ")
     else:
         print(i, end =" ")
