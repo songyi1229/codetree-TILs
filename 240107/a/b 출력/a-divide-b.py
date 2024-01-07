59af2d6749d05267ce703cb78321a53be2f0139e
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-01-07 23:20:37 +0900

    Title: a/b 출력, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240107/a/b 출력/a-divide-b.py
+++ 240107/a/b 출력/a-divide-b.py
@@ -1,8 +1,10 @@
-a, b = map(int, input().split())  #변수 선언 및 입력
+a,b = map(int, input().split())
 
-print(f"{a//b}.",end="")   #정수 부분 출력 
-a %= b   #a를 b로 나눈 후 나머지를 a에 할당
-
-for _ in range(20):  #소수점 20째자리 하나씩 계산
-    print((a*10)//b, end="")   #나머지에 10을 곱한 값을 b로 나눈 몫 출력
-    a = (a*10) % b   #a를 b로 나눈 후 나머지 갱신
+inte = a//b
+print(f"{inte}.", end ="")
+a%=b
+for i in range(20):
+    a*=10
+    d=a//b
+    print(d, end ="")
+    a%=b
