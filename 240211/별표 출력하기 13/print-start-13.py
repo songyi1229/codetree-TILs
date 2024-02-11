a8b7f9cf78f5e9dc896bb1c125da3186d7110e3e
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-02-11 11:32:42 +0900

    Title: 별표 출력하기 13, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240211/별표 출력하기 13/print-start-13.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(2*n):
+    if i%2==0:
+        for _ in range(n-i//2):
+            print('*', end=" ")
+        print()
+    else:
+        for _ in range(i-i//2):
+            print('*', end=" ")
+        print()
