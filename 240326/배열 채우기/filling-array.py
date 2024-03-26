743042cb7b3f8bde4ccc8a8b4873bdbc6fa6ffb7
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-03-26 16:47:35 +0900

    Title: 배열 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240326/배열 채우기/filling-array.py
+++ 240326/배열 채우기/filling-array.py
@@ -4,8 +4,6 @@ arr = list(map(int, input().split()))
 for i in range(10):
     if arr[i] == 0:
         arr.pop()
-        arr = arr[::-1]
-    elif i==9:
-        arr = arr[::-1]
+arr = arr[::-1]
 for j in arr:
     print(j, end=" ")
