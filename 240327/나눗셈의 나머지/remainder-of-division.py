86cacf3e12c6b86be15174bbb552712cd35553e4
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-03-27 15:50:10 +0900

    Title: 나눗셈의 나머지, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240327/나눗셈의 나머지/remainder-of-division.py
+++ 240327/나눗셈의 나머지/remainder-of-division.py
@@ -4,12 +4,14 @@ sum_val = 0
 while True:
     if a<=1:
         break
-    a = a//b
     
     count[a%b] += 1
+    a = a// b
 
+
+    
 
 for i in count:
     sum_val += i**2
-    
-print(sum_val)
+
+print(f"{sum_val}")
