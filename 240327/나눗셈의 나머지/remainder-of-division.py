a9add19ba5ae41a1e5a20eee3a62e42692af193f
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-03-27 13:45:09 +0900

    Title: 나눗셈의 나머지, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240327/나눗셈의 나머지/remainder-of-division.py
@@ -0,0 +1,15 @@
+a, b = map(int, input().split())
+count = [0]*10
+sum_val = 0
+while True:
+    if a<=1:
+    break
+    a = a//b
+    
+    count[a%b] += 1
+
+
+for i in count:
+    sum_val += i**2
+    
+print(sum_val)
