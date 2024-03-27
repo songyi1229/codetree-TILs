e2a5db21f62327bf19912d9e3588a55f8ab5351f
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-03-27 18:21:20 +0900

    Title: 연속부분수열일까, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240327/연속부분수열일까/contiguous-array-or-not.py
@@ -0,0 +1,20 @@
+a, b = tuple(map(int, input().split()))
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+cnt = [0]*(len(B)+1)  # A의 인덱스 저장
+
+for i in range(len(B)):
+    if B[i] in A:
+        idx = A.index(B[i])
+    cnt[i]=idx + 1
+
+# for j in range(len(cnt)):
+#     if cnt[j+1] - cnt[j] == 1:
+#         print('No')
+#     else:
+#         print('Yes')
+if cnt[1] - cnt[0] == 1:
+    print('Yes')
+else:
+    print('No')
