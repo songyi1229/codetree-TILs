4816673d58b6ff03a10edc6459fa4974177c80b2
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-04-01 23:34:01 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 129ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240401/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -0,0 +1,13 @@
+n = int(input())
+price = list(map(int, input().split()))
+
+# 배열을 앞에서부터 순회하며 사는 시점의 후보 선택
+max_profit = 0
+
+for i in range(n): # 사는 시점의 후보
+    for j in range(i+1, n):
+        profit = price[j] - price[i]
+        if profit > max_profit:
+            max_profit = profit
+        
+print(max_profit)
