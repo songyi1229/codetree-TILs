315e43c56b3cd78de925858b395209a32e451cc2
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-04-02 00:00:15 +0900

    Title: 대문자로 바꾸기, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240402/대문자로 바꾸기/change-to-capital.py
@@ -0,0 +1,17 @@
+arr_2d = [
+    list(input().split())
+    for _ in range(5)
+]
+
+# for i in range(5):
+# 	for j in range(3):
+# 		arr_2d[i][j] = chr(ord(arr_2d[i][j]) + ord('A') - ord('a'))
+# 		print(arr_2d[i][j], end=" ")
+# 	print()
+
+
+
+for i in range(5):
+	for j in range(3):
+		print(arr_2d[i][j].upper(), end=" ")
+	print()
