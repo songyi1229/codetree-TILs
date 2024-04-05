4dfcc5b0c1ee5c9263dd9ceb910bd27ccdfa2c9e
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-04-05 22:03:27 +0900

    Title: 최대공약수 구하기, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240405/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 240405/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -1,9 +1,12 @@
-n, m = tuple(map(int, input().split()))
+def find_max(n,m):
 
-max_val = 0
+    max_val = 0
 
-for i in range(1,101):
-    if n%i==0 and m%i==0:
-        max_val = i
-    
-print(max_val)
+    for i in range(1,101):
+        if n%i==0 and m%i==0:
+            max_val = i
+        
+    print(max_val)
+
+n_num, m_num = tuple(map(int, input().split()))
+find_max(n_num, m_num)
