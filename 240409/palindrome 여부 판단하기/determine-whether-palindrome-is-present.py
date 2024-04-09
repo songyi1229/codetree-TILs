f7bec587701e4560fc259c386f30e54a67054376
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-04-09 23:13:47 +0900

    Title: palindrome 여부 판단하기, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240409/palindrome 여부 판단하기/determine-whether-palindrome-is-present.py
@@ -0,0 +1,12 @@
+string = input()
+
+def palindrome(s):
+    for i in range(len(s)):
+        if s[i] != s[len(s)-i-1]:
+            return False
+    return True
+
+if palindrome(string):
+    print('Yes')
+else:
+    print('No')
