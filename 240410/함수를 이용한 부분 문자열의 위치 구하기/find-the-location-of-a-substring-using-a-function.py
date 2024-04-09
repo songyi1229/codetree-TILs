038c0b5b4748cb08db299e641953516e09b7e531
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-04-10 01:05:10 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240410/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -0,0 +1,23 @@
+arr1 = input()
+arr2 = input()
+n1 = len(arr1)
+n2 = len(arr2)
+
+
+def is_same(n):
+    for i in range(n2):
+        if arr1[n+i] != arr2[i]:
+            return False
+    return True
+
+
+def is_seq():
+    for i in range(n1-n2+1):
+        if is_same(i):
+            return i
+    return False
+
+if is_seq():
+    print(is_seq())
+else:
+    print(-1)
