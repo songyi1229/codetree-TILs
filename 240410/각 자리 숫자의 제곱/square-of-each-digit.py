67c80b4afe97f2f183ea35920432981e863beab9
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-04-10 15:58:29 +0900

    Title: 각 자리 숫자의 제곱, Time: 163ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240410/각 자리 숫자의 제곱/square-of-each-digit.py
@@ -0,0 +1,16 @@
+# def f(n):
+#     if n<10:
+#         return n
+#     return(f(n//10) + f(n%10))
+
+
+n = int(input())
+
+def f(n):
+    if n<10:
+        return n**2
+
+
+    return ((f(n//10)) + (n%10)**2)
+
+print(f(n))
