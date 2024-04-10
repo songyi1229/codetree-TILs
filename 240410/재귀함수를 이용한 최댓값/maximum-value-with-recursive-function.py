f16a1532cfeca956bb9d3c9f5a85b9588edd92b2
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-04-10 19:50:23 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 240410/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
+++ 240410/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -1,10 +1,14 @@
+# 변수 선언 및 입력:
 n = int(input())
 arr = list(map(int, input().split()))
 
-def find_max(n):
-    if n==0:
-        return
 
-    return max(arr)
+# a번째 까지 인덱스의 숫자 중에 가장 큰 값을 반환합니다.
+def max_value(a):
+    if a == 0:
+        return arr[0]
 
-print(find_max(n))
+    return max(max_value(a - 1), arr[a])
+
+
+print(max_value(n - 1))
