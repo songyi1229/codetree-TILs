76e691f1cae75c3e9f5ce63d72c85d329f5f0e9b
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-04-11 14:54:45 +0900

    Title: 일렬로 서있는 소 2, Time: 114ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240411/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -0,0 +1,12 @@
+n = int(input())
+height = list(map(int, input().split()))
+
+cnt = 0
+
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            if (height[i] <= height[j]) and (height[j] <= height[k]):
+                cnt += 1
+
+print(cnt)
