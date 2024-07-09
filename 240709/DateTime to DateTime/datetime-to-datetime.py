8943cf38a216da0ccd8418b0b8b8a17d4d9c68fd
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-07-09 15:21:21 +0900

    Title: DateTime to DateTime, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240709/DateTime to DateTime/datetime-to-datetime.py
+++ 240709/DateTime to DateTime/datetime-to-datetime.py
@@ -8,7 +8,7 @@ def timetomin(h, m):
 
     return (h*60 + m)-(11*60+11)
 
-if a<11 or (a<=11 and b<11) or (a<=11 or b<=11 or c<11):
+if a<=11 and b<=11 and c<=11:
     print("-1")
 else:
     print(daytomin(a) + timetomin(b, c))
