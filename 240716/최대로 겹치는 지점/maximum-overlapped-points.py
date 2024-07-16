b12292bed94b9b2006c4b28d9d7ac25feabf95de
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-07-16 18:44:25 +0900

    Title: 최대로 겹치는 지점, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240716/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -0,0 +1,16 @@
+# 끝점에서 닿는 경우에도 겹치는 것으로 보기 때문에 x1~x2
+
+n = int(input())
+
+ary = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+    ]
+
+zero_ary = [0] * 100
+
+for a, b in ary:
+    for i in range(a, b+1):
+        zero_ary[i] += 1
+
+print(max(zero_ary))
