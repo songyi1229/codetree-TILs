58caa9f2e4d6790cd5c64c6da75949cb0066591b
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-08-27 18:46:39 +0900

    Title: 벌금은 누구에게, Time: 137ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240827/벌금은 누구에게/who-will-pay.py
+++ 240827/벌금은 누구에게/who-will-pay.py
@@ -9,9 +9,9 @@ for _ in range(1, m):
     lst[num] += 1
     num = 0
 
+ans = -1
 for a,b in enumerate(lst):
-    if k not in lst:
-        break
-    elif b == k:
-        print(a)
-    print(-1)
+    if b >= k:
+        ans = a
+
+print(ans)
