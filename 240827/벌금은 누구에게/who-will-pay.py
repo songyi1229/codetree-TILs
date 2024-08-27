44cacea9be2384fb264c53b89d36efb3cce61646
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-08-27 18:57:12 +0900

    Title: 벌금은 누구에게, Time: 148ms, Memory: 25MB, Status: Passed - CodeTree

--- 240827/벌금은 누구에게/who-will-pay.py
+++ 240827/벌금은 누구에게/who-will-pay.py
@@ -1,21 +1,9 @@
 # n:학생수 m:벌칙에 걸린 학생 수 k:벌금 내야하는 벌칙 수
 
 
-n, m, k = map(int, input().split())
+n, m, k = tuple(map(int, input().split()))
 lst = [0]*(n+1)
 
-for _ in range(1, m):
-    num = int(input())
-    lst[num] += 1
-    num = 0
-
-ans = -1
-for i in lst:
-    if lst[i] >= k:
-        ans = i
-        break
-
-print(ans)
 
 
 ary = [
