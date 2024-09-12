8cf72a9eacf5c4a7bf1e751b5f5c7ef551dd2427
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-09-12 16:30:23 +0900

    Title: G or H 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240912/G or H 3/G-or-H-3.py
+++ 240912/G or H 3/G-or-H-3.py
@@ -4,11 +4,13 @@
 n, k = tuple(map(int, input().split()))
 
 placed = [0] * 101
+arr = [0] * (n+1)
 
 max_elem = 0
 for _ in range(n):
     elem, point = input().split()
     elem = int(elem)
+    arr.append(elem)
     if point == 'G':
         placed[elem] = 1
     else:
@@ -16,12 +18,15 @@ for _ in range(n):
     if elem > max_elem:
         max_elem = elem
 
+arr.sort()
 
 ans = 0
-for i in range(max_elem - k + 1):
+
+for i in arr:
     max_score = 0
     for j in range(i, i+k+1):
         max_score += placed[j]
         ans = max(ans, max_score)
+
 
 print(ans)
