373767837c974fe420f908f004058ce46db07d53
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-10-02 21:57:28 +0900

    Title: 개발자의 능력 2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241002/개발자의 능력 2/ability-of-developer-2.py
@@ -0,0 +1,34 @@
+arr = tuple(map(int, input().split()))
+
+
+def get_team(x,y,z,w):
+    team1 = arr[x] + arr[y]
+    team2 = arr[z] + arr[w]
+    team3 = sum(arr) - team1 - team2
+    
+    team_arr = [team1, team2, team3]
+    max_team = max(team_arr)
+    min_team = min(team_arr)
+    return (max_team - min_team)
+
+min_val = 100
+
+#첫번째 팀원
+for i in range(6):
+    for j in range(i+1, 6):
+
+        # 두번째 팀원 (처음부터 새로 시작) 하지만 첫번쨰 팀원과 겹치는 인원이 있으면 안됨
+        for k in range(6):
+            for l in range(k+1, 6):
+                if k==i or k==j or l==i or l==j:
+                    continue
+
+                diff = get_team(i,j,k,l)
+
+                if diff < min_val:
+                    min_val = diff
+
+                # min_val = min(min_val, get_team(i,j,k,l))
+
+
+print(min_val)
