2d151d02089706903f6b5c5936be4d325c00fc7e
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-10-03 14:52:52 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

--- 241003/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
+++ 241003/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
@@ -9,17 +9,16 @@ arr = [
 
 min_dist = sys.maxsize
 dist = 0
-min_dist_2 = 0
 
 for i in range(n):
     for j in range(i+1, n):
         x1,y1 = arr[i]
         x2,y2 = arr[j]
 
-        dist = (abs(x1+x2)*2 + abs(y1+y2)*2)*0.5
+        dist = abs(x1-x2)*abs(x1-x2) + abs(y1-y2)*abs(y1-y2)
         if dist < min_dist:
             min_dist = dist
-            min_dist_2 = abs(x1-x2)*abs(x1-x2) + abs(y1-y2)*abs(y1-y2)
+
             
 
-print(min_dist_2)
+print(min_dist)
