042693bffa004004f0a4cd1f6dab68f22e474431
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-10-09 23:13:08 +0900

    Title: 최고의 13위치 2, Time: 149ms, Memory: 26MB, Status: Passed - CodeTree

--- 241009/최고의 13위치 2/best-place-of-13-2.py
+++ 241009/최고의 13위치 2/best-place-of-13-2.py
@@ -9,8 +9,13 @@ max_cnt = 0
 
 for i in range(n):
     for j in range(n-2):
-        for k in range(i+1, n):
+        for k in range(n):
             for l in range(n-2):
-                max_cnt = max(max_cnt, arr[i][j] + arr[i][j+1]+ arr[i][j+2] + arr[k][l] + arr[k][l+1]+ arr[k][l+2])
+                 # 두 격자가 겹치면 넘어가기(조건: 같은 행에 있고, 격자 시작 열 좌표 차이가 2이내)
+                if i==k and abs(j-l)<=2:
+                    continue
+                cnt1 = arr[i][j] + arr[i][j+1]+ arr[i][j+2]
+                cnt2 = arr[k][l] + arr[k][l+1]+ arr[k][l+2]
+                max_cnt = max(max_cnt, cnt1 + cnt2)
 
 print(max_cnt)
