49b936c6de72707cda16fc267681bb5a2d27a25f
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-10-09 17:26:25 +0900

    Title: 체크판위에서 2, Time: 146ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 241009/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,19 @@
+n, m = tuple(map(int, input().split()))
+grid =[
+    input().split()
+    for _ in range(n)
+]
+
+#이동시에 행과 열이 전부 증가하도록
+# 모든 쌍을 다 잡기
+
+cnt = 0
+for i in range(1, n):
+    for j in range(1, m):
+        for k in range(i+1, n-1):
+            for l in range(j+1, m-1):
+                # 그 중 색깔이 전부 달라지는 경우에만 개수 세기
+                if grid[0][0] != grid[i][j] and grid[i][j] != grid[k][l] and grid[k][l] != grid[n-1][m-1]:
+                    cnt += 1
+
+print(cnt)
