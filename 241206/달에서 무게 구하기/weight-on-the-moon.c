93faebc3f9c084caa06485846089f9a6220783d1
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-12-06 12:04:25 +0900

    Title: 달에서 무게 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241206/달에서 무게 구하기/weight-on-the-moon.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int weight = 13;
+    double g = 0.165;
+    double result = weight * g;
+    printf("%d * %.6lf = %.6lf",weight, g, result);
+    return 0;
+}
+
