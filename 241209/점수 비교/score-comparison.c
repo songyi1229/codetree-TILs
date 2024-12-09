3de70cacc6f42de3d090d03917254f39778e5ab6
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-12-09 16:22:52 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241209/점수 비교/score-comparison.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a_m, a_e, b_m, b_e;
+
+    scanf("%d %d", &a_m, &a_e);
+    scanf("%d %d", &b_m, &b_e);
+
+    if (a_m >= b_m && a_e>=b_e) {
+        printf("1");
+    }
+    else
+        printf("0");
+
+    return 0;
+}
+
+
