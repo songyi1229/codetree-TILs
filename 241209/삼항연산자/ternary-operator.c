76ec117a494d5599dd9624cab2af6ac651d8ca01
song-yi <83946999+songyi1229@users.noreply.github.com>
2024-12-09 15:12:52 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241209/삼항연산자/ternary-operator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int score;
+
+    scanf("%d", &score);
+
+    if (score == 100){
+        printf("pass");
+    }
+    else 
+        printf("failure");
+
+    return 0;
+}
